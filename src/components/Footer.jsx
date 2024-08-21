b3ee36df1aed14cf4cf2c290e397e6a50001626a
Roy <royhayek27@gmail.com>
2024-08-21 12:01:42 +0300

    Worked on portfolio

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <>
+      <hr />
+      <div className="flex items-center py-8">
+        <p className="text-sm text-gray-600">© {new Date().getFullYear()} Roy's Portfolio</p>
+      </div>
+    </>
+  );
+};
+
+// display: flex;
+// justify-content: space-around;
+// align-items: center;
+// padding: 2rem 0;
+
+export default Footer;
