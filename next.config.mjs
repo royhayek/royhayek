15fa0ff1fab3edf6fce73a5a5fc3a57a5f2c4d3c
Roy <royhayek27@gmail.com>
2024-08-21 12:29:04 +0300

    Made a build branch

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/royhayek",
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
-};
-
-export default nextConfig;
